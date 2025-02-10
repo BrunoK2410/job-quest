18ad867d59a753147b016ad2b88a5722799b2299
Bruno <brunokatic2410@gmail.com>
2025-02-10 13:57:18 +0100

    supabase instead of localhost

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,24 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::statement('ALTER TABLE job_listings DISABLE TRIGGER ALL;');
-        DB::statement('ALTER TABLE users DISABLE TRIGGER ALL;');
+      
         //Truncate tables
         DB::table('job_listings')->delete();
         DB::table('users')->delete();
         DB::table('job_user_bookmarks')->delete();
         DB::table('applicants')->delete();
 
-        DB::statement('ALTER TABLE job_listings ENABLE TRIGGER ALL;');
-        DB::statement('ALTER TABLE users ENABLE TRIGGER ALL;');
-        DB::statement('ALTER TABLE job_user_bookmarks ENABLE TRIGGER ALL;'); 
-        DB::statement('ALTER TABLE applicants ENABLE TRIGGER ALL;'); 
-
-        DB::statement('ALTER SEQUENCE users_id_seq RESTART WITH 1;');
-        DB::statement('ALTER SEQUENCE job_listings_id_seq RESTART WITH 1;');
-        DB::statement('ALTER SEQUENCE job_user_bookmarks_id_seq RESTART WITH 1;');
-        DB::statement('ALTER SEQUENCE applicants_id_seq RESTART WITH 1;');
-
+    
         $this->call(TestUserSeeder::class);
         $this->call(RandomUserSeeder::class);
         $this->call(JobSeeder::class);
